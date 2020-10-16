69d06a632815ae02d1a4c8eee39c91eafa21e1ce
amirrezaask <raskarpour@gmail.com>
2020-10-16 12:39:32 +0330

    some improvements to MusicPlayerController

--- lua/music_player/init.lua
+++ lua/music_player/init.lua
@@ -16,6 +16,9 @@ vim.cmd [[ command! MPToggle lua require'music_player'.backend().play_pause(requ
 vim.cmd [[ command! MPPrev lua require'music_player'.backend().prev_song(require'music_player.backend()) ]]
 
 local function update_buf(buf, lines)
+  table.insert(lines, "Press <Enter> for toggling between play and pause")
+  table.insert(lines, "Press <n> for next song")
+  table.insert(lines, "Press <p> for previous song")
   api.nvim_buf_set_option(buf, 'modifiable', true)
   api.nvim_buf_set_lines(buf, 0, -1, false, lines) 
   api.nvim_buf_set_option(buf, 'modifiable', false)
@@ -24,8 +27,10 @@ end
 
 local function create_control_buffer(backend)
   local buf = api.nvim_create_buf(true, false)
+  api.nvim_buf_set_name(buf, 'MusicPlayerController')
   api.nvim_buf_set_option(buf, 'modifiable', false)
   api.nvim_buf_set_option(buf, 'buftype', 'nowrite')
+
   current_backend = backend 
   -- Set keymaps
   api.nvim_buf_set_keymap(buf, 'n', 'n', '<cmd>lua require"music_player".backend().next_song(require"music_player".backend())<CR>', {})
